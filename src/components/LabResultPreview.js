20fcefda39b8375e5124bdf23498e331c8e2e466
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2023-11-05 18:21:09 +0630

    update logo state2

--- src/components/LabResultPreview.js
+++ src/components/LabResultPreview.js
@@ -383,7 +383,7 @@ const LabResultPreview = (
               </TableBody>
             </Table>
           </TableContainer>
-          {logoState? (
+          {logoState&& (
             <div
               style={{
                 width: "100%",
@@ -393,48 +393,10 @@ const LabResultPreview = (
                 justifyContent: "flex-end",
               }}
             >
-              {/* {stampState && (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <p
-                    style={{
-                      marginBottom: "3px",
-                      font: "bold",
-                      fontSize: "0.9rem",
-                      color: "blue",
-                    }}
-                  >
-                    Daw Yin Minn Thu
-                  </p>
-                  <p
-                    style={{
-                      marginTop: "3px",
-                      font: "bold",
-                      fontSize: "0.9rem",
-                      color: "blue",
-                    }}
-                  >
-                    B.Med.Tech (Laboratory)
-                  </p>
-                </div>
-              )} */}
-            </div>
-          ):
-          (
-            <div
-              style={{
-                width: "100%",
-                height: "4rem",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-end",
-              }}
-            >
-              {stampState && (
+              {stampState? (
+                <div></div>
+              ):
+              (
                 <div
                   style={{
                     display: "flex",
